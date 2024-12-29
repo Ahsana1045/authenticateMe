5c9cf012a118224a4cabc8bca45e8a44b0b3f6db
Ali <ahsana1045@gmail.com>
2024-12-28 20:20:10 -0800

    Successfully added a Spot

---
+++ backend/db/models/spot.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Spot belongs to User (ownerId)
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId',
+        as: 'owner',
+      });
+
+      // // Spot has many Reviews
+      // Spot.hasMany(models.Review, {
+      //   foreignKey: 'spotId',
+      //   as: 'reviews',
+      // });
+
+      // // Spot has many SpotImages
+      // Spot.hasMany(models.SpotImage, {
+      //   foreignKey: 'spotId',
+      //   as: 'spotImages',
+      // });
+    }
+  }
+
+  Spot.init({
+    ownerId: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    country: DataTypes.STRING,
+    lat: DataTypes.DECIMAL,
+    lng: DataTypes.DECIMAL,
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.DECIMAL
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+
+  return Spot;
+};
---
+++ backend/db/seeders/20241229005909-demo-spot.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Spots', [
+      {
+        ownerId: 7,
+        address: '123 Disney Lane',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'United States of America',
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: 'App Academy',
+        description: 'Place where web developers are created',
+        price: 123.45,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        ownerId: 8,
+        address: '12 choram',
+        city: 'San jersey',
+        state: 'NY',
+        country: 'United States of America',
+        lat: 50.7645358,
+        lng: -20.4730327,
+        name: 'Long Island',
+        description: 'Place where trains are created',
+        price: 200.45,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Spots', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
