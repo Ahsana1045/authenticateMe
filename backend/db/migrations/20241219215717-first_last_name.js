dbc1dcabb238009127f627388b6dcda68eb48d83
Ali <ahsana1045@gmail.com>
2024-12-30 02:56:22 -0800

    default value in firstlast name migration corrected

--- backend/db/migrations/20241219215717-first_last_name.js
+++ backend/db/migrations/20241219215717-first_last_name.js
@@ -3,12 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
     await queryInterface.addColumn('Users', 'firstName', {
         allowNull: false,
         type: Sequelize.STRING,
@@ -19,16 +14,11 @@ module.exports = {
         allowNull: true,
         type: Sequelize.STRING,
         unique: false,
-        defalutValue: "NA"});
+        defaultValue: "NA"});
 },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
 
       await queryInterface.removeColumn('Users', 'firstName');
       await queryInterface.removeColumn('Users', 'lastName');
