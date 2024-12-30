21f2b917dfc480a4f9ddc234d75dc1b1a89ad385
Ali <ahsana1045@gmail.com>
2024-12-30 14:59:51 -0800

    spotId updated in seeders now available

--- backend/db/seeders/20240101-demo-spotimage.js
+++ backend/db/seeders/20240101-demo-spotimage.js
@@ -1,17 +1,19 @@
 'use strict';
 
+const spotimage = require("../models/spotimage");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('SpotImages', [
       {
-        // spotId: 3, // The ID of the spot you're associating this image with
+        spotId: 3, // The ID of the spot you're associating this image with
         url: 'https://example.com/spot-image.jpg', // URL for the spot image
         preview: true, // Whether this is the preview image
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        // spotId: 2,
+        spotId: 2,
         url: 'https://example.com/another-spot-image.jpg',
         preview: false,
         createdAt: new Date(),
