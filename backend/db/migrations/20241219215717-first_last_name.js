6ab79267f8773e5d9067d03acb65a6a20f11d489
Ali <ahsana1045@gmail.com>
2024-12-30 09:29:52 -0800

    added code into firstlast name migrations that were commented out

--- backend/db/migrations/20241219215717-first_last_name.js
+++ backend/db/migrations/20241219215717-first_last_name.js
@@ -1,3 +1,43 @@
+// const schema = process.env.SCHEMA || 'public';
+
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.addColumn(
+//       { tableName: 'Users', schema },
+//       'firstName',
+//       {
+//         allowNull: false,
+//         type: Sequelize.STRING,
+//         unique: false,
+//         defaultValue: "NA",
+//       }
+//     );
+
+//     await queryInterface.addColumn(
+//       { tableName: 'Users', schema },
+//       'lastName',
+//       {
+//         allowNull: true,
+//         type: Sequelize.STRING,
+//         unique: false,
+//         defaultValue: "NA", // Fixed spelling
+//       }
+//     );
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.removeColumn(
+//       { tableName: 'Users', schema },
+//       'firstName'
+//     );
+//     await queryInterface.removeColumn(
+//       { tableName: 'Users', schema },
+//       'lastName'
+//     );
+//   },
+// };
+
+
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
