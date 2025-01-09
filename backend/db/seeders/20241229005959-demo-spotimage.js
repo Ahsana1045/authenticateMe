c79e2c52e490979b834093d650a40be796677343
Ali <ahsana1045@gmail.com>
2025-01-08 18:14:58 -0800

    all seeders updated with options object

--- backend/db/seeders/20241229005959-demo-spotimage.js
+++ backend/db/seeders/20241229005959-demo-spotimage.js
@@ -2,36 +2,41 @@
 require('dotenv').config();
 
 const { SpotImage } = require('../models');
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
-const spotimage = require("../models/spotimage");
+// const spotimage = require("../models/spotimage");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('SpotImages', [
+    options.tableName = "SpotImages"
+    return queryInterface.bulkInsert(options, [
       {
-        spotId: 1, // The ID of the spot you're associating this image with
-        url: 'https://example.com/spot-image.jpg', // URL for the spot image
+        spotId: 1, // The ID of the spot associating this image w
+        url: 'https://example1.com/spot-image.jpg', // URL for the spot image
         preview: true, // Whether this is the preview image
         createdAt: new Date(),
         updatedAt: new Date()
       },
-      // {
-      //   spotId: 2,
-      //   url: 'https://example.com/another-spot-image.jpg',
-      //   preview: false,
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }
+      {
+        spotId: 2,
+        url: 'https://example2.com/another-spot-image.jpg',
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('SpotImages', null, {});
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2] }
+    }, null);
   }
 };
--- backend/db/seeders/20241229022623-review-data.js
+++ backend/db/seeders/20241229022623-review-data.js
@@ -3,36 +3,58 @@
 require('dotenv').config();
 
 const { Review } = require('../models');
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
-
-// let options = {};
-// if (process.env.NODE_ENV === 'production') {
-//   options.schema = process.env.SCHEMA;  // define your schema in options object
-// }
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await Review.bulkCreate([
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    return queryInterface.bulkInsert(options, [
       {
         userId: 1,
         spotId: 1,
         review: 'What a great movie!',
         stars: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], { validate: true });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       userId: { [Op.in]: [1] }
-    }, {});
+    }, null);
   }
 };
+
+
+
+///OLDERRRRR
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+
+//     await Review.bulkCreate([
+//       {
+//         userId: 1,
+//         spotId: 1,
+//         review: 'What a great movie!',
+//         stars: 5,
+//       }
+//     ], { validate: true });
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     options.tableName = 'Reviews';
+//     const Op = Sequelize.Op;
+//     return queryInterface.bulkDelete(options, {
+//       userId: { [Op.in]: [1] }
+//     }, {});
+//   }
+// };
