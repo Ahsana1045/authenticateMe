f642cf250b47d704eefb43e589e3842dcc28c615
Ali <ahsana1045@gmail.com>
2025-01-13 00:14:59 -0800

    LoginForm working

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,7 +16,10 @@ export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
     method: "POST",
-    body: JSON.stringify({ credential, password })
+    body: JSON.stringify({
+        credential,
+        password
+    })
   });
   const data = await response.json();
   dispatch(setUser(data.user));
