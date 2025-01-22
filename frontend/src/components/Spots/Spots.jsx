2b3519feb30fd847be513d660195638d81655257
Ali <ahsana1045@gmail.com>
2025-01-22 14:47:43 -0800

    Login User Working. Logout not working

--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -17,7 +17,7 @@ const Spots = () => {
   }, [dispatch]);
 
   // Check if allSpots is defined and has spots
-  console.log("SPOTSSSSSS: ", spots);
+  // console.log("SPOTSSSSSS: ", spots);
 
 
   // const handleDelete = (spotId) => {
@@ -33,7 +33,7 @@ const Spots = () => {
     <>
     <section>
     {/* <SearchBar /> */}
-    <hr />
+    {/* <hr /> */}
       <ul>
         <div className="spots-container">
           {spots.map((spot) => {
@@ -47,3 +47,69 @@ const Spots = () => {
 };
 
 export default Spots;
+
+
+// //With PAGINATION
+// import SpotItems from "./SpotItems";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getAllSpots } from "../../store/spotsReducer";
+// import { useEffect, useState } from "react";
+
+// import "./Spots.css";
+
+// const Spots = () => {
+//   const spots = useSelector((state) => Object.values(state.spots.allSpots));
+//   const dispatch = useDispatch();
+
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const spotsPerPage = 8; // 4 spots per row * 2 rows
+
+//   useEffect(() => {
+//     dispatch(getAllSpots());
+//   }, [dispatch]);
+
+//   // Pagination logic
+//   const indexOfLastSpot = currentPage * spotsPerPage;
+//   const indexOfFirstSpot = indexOfLastSpot - spotsPerPage;
+//   const currentSpots = spots.slice(indexOfFirstSpot, indexOfLastSpot);
+
+//   const totalPages = Math.ceil(spots.length / spotsPerPage);
+
+//   // Handlers for pagination buttons
+//   const handleNextPage = () => {
+//     if (currentPage < totalPages) {
+//       setCurrentPage(currentPage + 1);
+//     }
+//   };
+
+//   const handlePreviousPage = () => {
+//     if (currentPage > 1) {
+//       setCurrentPage(currentPage - 1);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <section>
+//         <div className="spots-container">
+//           {currentSpots.map((spot) => (
+//             <SpotItems spot={spot} key={spot.id} />
+//           ))}
+//         </div>
+//         <div className="pagination-controls">
+//           <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+//             Previous
+//           </button>
+//           <span>
+//             Page {currentPage} of {totalPages}
+//           </span>
+//           <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+//             Next
+//           </button>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Spots;
